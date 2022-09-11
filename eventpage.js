e7c1e8bf1f298b5f2aea394ec0b038a418a43b37
MIGUEL <msabl005@fiu.edu>
2022-09-11 09:57:31 -0400

    home and events pages

---
+++ eventpage.js
@@ -0,0 +1,15 @@
+// grab the anchor tag that was triggered
+var a = document.getElementById("eventSelected");
+
+print(a);
+
+/*
+// select the title
+let title = document.getElementById("event-title");
+print(title);
+
+// when the user clicks on the event card
+a.onclick = function () {
+  print(title);
+};
+*/
